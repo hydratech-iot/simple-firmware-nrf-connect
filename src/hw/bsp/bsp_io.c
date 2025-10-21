93c6eb8bab9ac125864eb738bc7878e493f04cbd
hieuha <hieuadai@gmail.com>
2025-10-21 16:13:30 +0700

    fix: Correct CS pin reference in bsp_io_init function

--- src/hw/bsp/bsp_io.c
+++ src/hw/bsp/bsp_io.c
@@ -53,7 +53,7 @@ void bsp_io_init(void)
     bsp_io_set_config_pin(&IO_TABLE[IO_PIN_LED].io_dt, GPIO_OUTPUT);
 
     // CS pin setting
-    bsp_io_set_config_pin(&IO_TABLE[IO_SPI_CS].io_dt, GPIO_OUTPUT);
+    bsp_io_set_config_pin(&IO_TABLE[IO_PIN_SPI_CS].io_dt, GPIO_OUTPUT);
 
     // Turn off the LED
     bsp_io_write_specific_pin(IO_PIN_LED, 0);
